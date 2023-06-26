7253882c70caf59f162ba71e74d6f281fbbbe735
Mridul <mridulsinghal474@gmail.com>
2023-06-26 23:11:54 +0530

    Added Alembic

---
+++ alembic/versions/e4ed6c55e1bb_add_user_table.py
@@ -0,0 +1,24 @@
+"""add user table
+
+Revision ID: e4ed6c55e1bb
+Revises: 929eb15b3ddb
+Create Date: 2023-06-26 23:07:43.551003
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e4ed6c55e1bb'
+down_revision = '929eb15b3ddb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
