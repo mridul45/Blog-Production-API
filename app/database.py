b3bb3543ab961c8e9378c4bc5154bf61d1032573
Mridul <mridulsinghal474@gmail.com>
2023-06-26 20:34:38 +0530

    New Changes

--- app/database.py
+++ app/database.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from .config import settings
 
 # from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:7015477816ms@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ from . import models,schemas,utils
 from .database import *
 from sqlalchemy.orm import Session
 from typing import List
-from .routers import post,user,auth
-
+from .routers import post,user,auth,vote
+from .config import settings
 
 
 app = FastAPI()
@@ -20,3 +20,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
